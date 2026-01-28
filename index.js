ca1c2acd149eedb51c7565764d53368c936eafa7
vakulenkoroman50-web <vakulenkoroman50@gmail.com>
2026-01-28 11:31:58 +0200

    Fix fetch

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const cors = require('cors');
 const crypto = require('crypto');
-const fetch = require('node-fetch');
 
 const app = express();
 app.use(cors());
